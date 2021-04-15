eb286da4611e09793f82438866f851bcb744c640
aakashbansal837 <aakashbansal837@gmail.com>
2021-04-15 20:36:34 +0530

    + routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./styles/index.scss";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./components/Home";
+import SignIn from "./components/SignIn";
+import SignUp from "./components/SignUp";
 
 function App() {
-  return <div className="App">this is main screen</div>;
+  return (
+    <div className="App">
+      <Router>
+        <Route exact path="/" component={Home}></Route>
+        <Route exact path="/signin" component={SignIn}></Route>
+        <Route exact path="/signup" component={SignUp}></Route>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
