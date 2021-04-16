5592951a54864d640a95e185356f29ba5df8732c
aakashbansal837 <aakashbansal837@gmail.com>
2021-04-16 23:10:12 +0530

    + profile

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import "./styles/index.scss";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import SignUp from "./components/SignUp";
 import LandingPage from "./components/LandingPage";
+import Profile from "./components/Profile";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Route exact path="/" component={LandingPage}></Route>
-        <Route exact path="/profile" component={SignUp}></Route>
+        <Route exact path="/profile" component={Profile}></Route>
       </Router>
     </div>
   );
