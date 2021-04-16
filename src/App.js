f8ebf7310b9f4de6ba35b0dabbcfa9e6d795f6f1
aakashbansal837 <aakashbansal837@gmail.com>
2021-04-16 22:23:11 +0530

    ^ routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import "./styles/index.scss";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Home from "./components/Home";
-import SignIn from "./components/SignIn";
 import SignUp from "./components/SignUp";
+import LandingPage from "./components/LandingPage";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Route exact path="/" component={Home}></Route>
-        <Route exact path="/signin" component={SignIn}></Route>
-        <Route exact path="/signup" component={SignUp}></Route>
+        <Route exact path="/" component={LandingPage}></Route>
+        <Route exact path="/profile" component={SignUp}></Route>
       </Router>
     </div>
   );
