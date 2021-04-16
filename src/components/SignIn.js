f1491321c3ecd3d8dc9c8aada93e2adb879c46f9
aakashbansal837 <aakashbansal837@gmail.com>
2021-04-17 02:47:18 +0530

    ^ store props

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,128 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { signInUser } from "../redux/actions/userAction";
 
 const SignIn = (props) => {
+  // user data
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
   // label focus
   const [focusInput, setFocusInput] = React.useState(false);
 
   // error states
-  const [usernameError, setUserNameError] = React.useState(false);
   const [emailError, setEmailError] = React.useState(false);
   const [passwordError, setPasswordError] = React.useState(false);
-  const [passConfirm, setPassConfirm] = React.useState(false);
 
   const validateInput = () => {
-    document.querySelector("input").blur(function () {
-      // User Name
-      if (document.querySelector(this).classList.contains("name")) {
-        if (document.querySelector(this).value.length === 0) {
-          document
-            .querySelector(this)
-            .siblings("span.error")
-            .text("Please type your full name")
-            .fadeIn()
-            .parent(".form-group")
-            .classList.add("hasError");
-          usernameError = true;
-        } else if (
-          document.querySelector(this).value.length > 1 &&
-          document.querySelector(this).value.length <= 6
-        ) {
-          document
-            .querySelector(this)
-            .siblings("span.error")
-            .text("Please type at least 6 characters")
-            .fadeIn()
-            .parent(".form-group")
-            .classList.add("hasError");
-          usernameError = true;
-        } else {
-          document
-            .querySelector(this)
-            .siblings(".error")
-            .text("")
-            .fadeOut()
-            .parent(".form-group")
-            .removeClass("hasError");
-          usernameError = false;
-        }
-      }
-      // Email
-      if (document.querySelector(this).classList.contains("email")) {
-        if (document.querySelector(this).value.length == "") {
-          document
-            .querySelector(this)
-            .siblings("span.error")
-            .text("Please type your email address")
-            .fadeIn()
-            .parent(".form-group")
-            .classList.add("hasError");
-          emailError = true;
-        } else {
-          document
-            .querySelector(this)
-            .siblings(".error")
-            .text("")
-            .fadeOut()
-            .parent(".form-group")
-            .removeClass("hasError");
-          emailError = false;
-        }
-      }
-
-      // PassWord
-      if (document.querySelector(this).classList.contains("pass")) {
-        if (document.querySelector(this).value.length < 8) {
-          document
-            .querySelector(this)
-            .siblings("span.error")
-            .text("Please type at least 8 charcters")
-            .fadeIn()
-            .parent(".form-group")
-            .classList.add("hasError");
-          passwordError = true;
-        } else {
-          document
-            .querySelector(this)
-            .siblings(".error")
-            .text("")
-            .fadeOut()
-            .parent(".form-group")
-            .removeClass("hasError");
-          passwordError = false;
-        }
-      }
-
-      // PassWord confirmation
-      if (
-        document.querySelector(".pass").value !==
-        document.querySelector(".passConfirm").value
-      ) {
-        document
-          .querySelector(".passConfirm")
-          .siblings(".error")
-          .text("Passwords don't match")
-          .fadeIn()
-          .parent(".form-group")
-          .classList.add("hasError");
-        passConfirm = false;
-      } else {
-        document
-          .querySelector(".passConfirm")
-          .siblings(".error")
-          .text("")
-          .fadeOut()
-          .parent(".form-group")
-          .removeClass("hasError");
-        passConfirm = false;
-      }
-
-      // label effect
-      if (document.querySelector(this).value.length > 0) {
-        document.querySelector(this).siblings("label").classList.add("active");
-      } else {
-        document.querySelector(this).siblings("label").removeClass("active");
-      }
-    });
     if (true) {
       setFocusInput(false);
     } else {
@@ -130,8 +22,17 @@ const SignIn = (props) => {
     }
   };
 
+  useEffect(() => {
+    if (props.is_logged_in) {
+      // props.activateProfile();
+    }
+  }, [props.is_logged_in]);
   const submitForm = () => {
-    props.activateProfile();
+    let data = {
+      email,
+      password,
+    };
+    props.signInUser(data);
   };
   const setLoginView = (value) => {
     props.setLoginView(value);
@@ -178,4 +79,11 @@ const SignIn = (props) => {
   );
 };
 
-export default SignIn;
+const mapStateToProps = (state) => ({
+  is_logged_in: state.user.is_logged_in,
+});
+const mapDispatchToProps = {
+  signInUser,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(SignIn);
