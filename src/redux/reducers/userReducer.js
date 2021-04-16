c923680a3fbb68ee3125119c268da7f1c0ff7f89
aakashbansal837 <aakashbansal837@gmail.com>
2021-04-17 02:48:16 +0530

    + login user reducer

--- src/redux/reducers/userReducer.js
+++ src/redux/reducers/userReducer.js
@@ -7,12 +7,28 @@ const initialState = {
 const userReducer = (state = initialState, action) => {
   switch (action.type) {
     case "NEW_USER":
+      let email = action.value.email;
       let tmp_user = state.users;
-      tmp_user.push(action.value);
-      return { ...state, users: tmp_user };
+      let tmp_emails = state.emails;
+      if (email in tmp_emails) {
+        return { ...state };
+      } else {
+        tmp_user[email] = action.value;
+        tmp_emails.push(email);
+      }
+
+      console.log("new data :", state);
+
+      return { ...state, users: tmp_user, emails: tmp_emails };
+    case "LOGIN_USER":
+      return {
+        ...state,
+        is_logged_in: true,
+      };
     case "LOGOUT_USER":
       return {
         ...state,
+        is_logged_in: false,
       };
     default:
       return state;
