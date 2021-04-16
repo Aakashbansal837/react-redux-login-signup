08abdba8e01c49d9f7f4e818a3cd6c3187e3c8fc
aakashbansal837 <aakashbansal837@gmail.com>
2021-04-17 04:49:15 +0530

    + new actions

--- src/redux/actions/userAction.js
+++ src/redux/actions/userAction.js
@@ -1,22 +1,50 @@
 import store from "../store";
 
 export const createNewUser = (value) => (dispatch) => {
-  console.log("value:", value);
-  dispatch({
-    type: "NEW_USER",
-    value: value,
-  });
+  let emails = store.getState().user.emails ? store.getState().user.emails : [];
+  let users = store.getState().user.users;
+  let email = value.email;
+
+  console.log({ emails });
+  if (emails.indexOf(email) !== -1) {
+    dispatch({
+      type: "SHOW_SNACKBAR",
+      message: "USER ALREADY EXISTS",
+      variant: "warning",
+    });
+  } else {
+    users[email] = value;
+    emails.push(email);
+    let data = {
+      emails: emails,
+      users: users,
+    };
+    dispatch({
+      type: "SHOW_SNACKBAR",
+      message: "NEW USER CREATION SUCCESSFUL",
+      variant: "success",
+    });
+    dispatch({
+      type: "NEW_USER",
+      value: data,
+    });
+    dispatch({
+      type: "LOGIN_USER",
+    });
+  }
 };
 
 export const signInUser = (value) => (dispatch) => {
-  let users = store.getState().user.users;
-  let emails = store.getState().user.emails;
-  console.log("data ", emails);
+  let users = store.getState().user.users ? store.getState().user.users : {};
+  let emails = store.getState().user.emails ? store.getState().user.emails : [];
   let email = value.email;
   let password = value.password;
 
-  if (email in emails) {
+  console.log({ emails });
+
+  if (emails.indexOf(email) !== -1) {
     let user = users[email];
+    console.log("user :", user);
     if (user.password === password) {
       dispatch({
         type: "SHOW_SNACKBAR",
@@ -38,4 +66,13 @@ export const signInUser = (value) => (dispatch) => {
       variant: "error",
     });
   }
+};
+
+export const signOutUser = (value) => (dispatch) => {
+  dispatch({
+    type: "SHOW_SNACKBAR",
+    message: "USER LOGGED OUT",
+    variant: "info",
+  });
+  dispatch({ type: "LOGOUT_USER" });
 };
