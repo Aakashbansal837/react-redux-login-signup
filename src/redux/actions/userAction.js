597b1c74c4e096e9f5a53d6e478acf0e7830fa64
aakashbansal837 <aakashbansal837@gmail.com>
2021-04-17 01:13:52 +0530

    + add new user action

--- src/redux/actions/userAction.js
+++ src/redux/actions/userAction.js
@@ -1,19 +1,7 @@
-export const addTodo = (value) => ({
-  type: "ADD_USER",
-});
-
-export const setVisibilityFilter = (filter) => ({
-  type: "SET_VISIBILITY_FILTER",
-  filter,
-});
-
-export const toggleTodo = (id) => ({
-  type: "TOGGLE_TODO",
-  id,
-});
-
-export const VisibilityFilters = {
-  SHOW_ALL: "SHOW_ALL",
-  SHOW_COMPLETED: "SHOW_COMPLETED",
-  SHOW_ACTIVE: "SHOW_ACTIVE",
+export const createNewUser = (value) => (dispatch) => {
+  console.log("value:", value);
+  dispatch({
+    type: "NEW_USER",
+    value: value,
+  });
 };
