deb6367d97a6d9f533dfd96a641b26ae89a1f133
aakashbansal837 <aakashbansal837@gmail.com>
2021-04-17 05:03:59 +0530

    + view profile

--- src/components/Brand.js
+++ src/components/Brand.js
@@ -1,11 +1,16 @@
 import React from "react";
+import { connect } from "react-redux";
+import { viewProfile } from "../redux/actions/userAction";
 
 const Brand = (props) => {
+  const showProfile = () => {
+    props.viewProfile();
+  };
+
   return (
     <div className="col-sm-6 brand">
       <a href="#" className="logo">
         Made By
-        {/* <span>.</span> */}
       </a>
 
       <div className="heading">
@@ -15,7 +20,7 @@ const Brand = (props) => {
 
       <div className="success-msg">
         <p>Great! You are one of our members now</p>
-        <a href="/profile" className="profile">
+        <a onClick={() => showProfile()} className="profile">
           Your Profile
         </a>
       </div>
@@ -23,4 +28,11 @@ const Brand = (props) => {
   );
 };
 
-export default Brand;
+const mapStateToProps = (state) => ({
+  is_logged_in: state.user.is_logged_in,
+});
+const mapDispatchToProps = {
+  viewProfile,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Brand);
