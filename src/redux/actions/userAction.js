16a57b9a8c54845247ed8e981472756403a4f880
aakashbansal837 <aakashbansal837@gmail.com>
2021-04-17 00:25:42 +0530

    + user actions

---
+++ src/redux/actions/userAction.js
@@ -0,0 +1,19 @@
+export const addTodo = (value) => ({
+  type: "ADD_USER",
+});
+
+export const setVisibilityFilter = (filter) => ({
+  type: "SET_VISIBILITY_FILTER",
+  filter,
+});
+
+export const toggleTodo = (id) => ({
+  type: "TOGGLE_TODO",
+  id,
+});
+
+export const VisibilityFilters = {
+  SHOW_ALL: "SHOW_ALL",
+  SHOW_COMPLETED: "SHOW_COMPLETED",
+  SHOW_ACTIVE: "SHOW_ACTIVE",
+};
