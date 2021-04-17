146c1b4f596ed8a4a5a3fb7f7364b91f15e4231f
aakashbansal837 <aakashbansal837@gmail.com>
2021-04-17 06:03:45 +0530

    value -> payload

--- src/redux/reducers/userReducer.js
+++ src/redux/reducers/userReducer.js
@@ -1,8 +1,20 @@
 const initialState = {
-  users: [],
-  emails: [],
+  users: {
+    "abc@abc.com": {
+      userName: "aakash",
+      email: "abc@abc.com",
+      password: "abc",
+    },
+  },
+  currentUser: {
+    userName: "aakash",
+    email: "abc@abc.com",
+    password: "abc",
+    image: "",
+  },
+  emails: ["abc@abc.com"],
   is_logged_in: false,
-  view_profile: false,
+  view_profile: true,
 };
 
 const userReducer = (state = initialState, action) => {
@@ -10,18 +22,20 @@ const userReducer = (state = initialState, action) => {
     case "NEW_USER":
       return {
         ...state,
-        users: action.value.users,
-        emails: action.value.emails,
+        users: action.payload.users,
+        emails: action.payload.emails,
       };
     case "LOGIN_USER":
       return {
         ...state,
         is_logged_in: true,
+        currentUser: action.payload,
       };
     case "LOGOUT_USER":
       return {
         ...state,
         is_logged_in: false,
+        view_profile: false,
       };
     case "VIEW_PROFILE":
       return {
